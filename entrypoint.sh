16b9f09019d2fb680939d8fe058489300e477a9d
Josip Radic <josip.radic@gmail.com>
2019-10-16 16:12:17 +0200

    Converting .pem into .crt before update-ca-certificates is called.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# convert certificates from .pem to .crt
+openssl x509 -outform der \
+    -in /usr/local/share/ca-certificates/ca.pem \
+    -out /usr/local/share/ca-certificates/ca.crt
+openssl x509 -outform der \
+    -in /usr/local/share/ca-certificates/cert.pem \
+    -out /usr/local/share/ca-certificates/client.crt
+openssl x509 -outform der \
+    -in /usr/local/share/ca-certificates/key.pem \
+    -out /usr/local/share/ca-certificates/client.key
+
 # update certificates
 sudo update-ca-certificates
 
