5442258b59a6f133404a3ed3a481c0de74f91e37
Josip Radic <josip.radic@gmail.com>
2020-02-28 10:38:57 -0300

    Kompose has been updated and the newest version includes all fixes that kompose-patch.sh script was applying. It seems that kompose-patch.sh is not needed anymore.

--- Dockerfile
+++ Dockerfile
@@ -60,7 +60,7 @@ RUN \
     echo "Installing kompose ..." && \
         go get -v -u github.com/kubernetes/kompose
 
-COPY kompose-patch.sh /usr/local/bin/kompose-patch
+COPY kompose-1.20.0-patch.sh /usr/local/bin/kompose-patch
 COPY knsk.sh /usr/local/bin/knsk
 RUN \
     echo "Installing kompose patch ..." && \
--- kompose-1.20.0-patch.sh
+++ kompose-1.20.0-patch.sh
@@ -2,10 +2,12 @@
 
 # until kompose is updated to support the kubernetes version v1.19 this script
 # is patching improperly generated configuration .yaml files and applies the
-# necessary information and changes
+# necessary information and changes.
 # 
 # @author Josip Radic <josip.radic@gmail.com><skype:josip.radich>
 # @version 2019/12/23
+# @version 2020/02/28 - kompose just got updated and it is covering all required
+# updates to the files that this patch has been doing.
 find * -type f -name "*-networkpolicy.yaml" -exec sed -i 's/extensions\/v1beta1/networking.k8s.io\/v1/g' {} \;
 find * -type f -name "*-podsecuritypolicy.yaml" -exec sed -i 's/extensions\/v1beta1/policy\/v1beta1/g' {} \;
 find * -type f -name "*-daemonset.yaml" -o -name "*-deployment.yaml" -exec sed -i 's/extensions\/v1beta1/apps\/v1/g' {} \;
